265de8a7131f80a621e2312fd38ca44c8b9b49b4
Agung Riyadi <ariadi01@gmail.com>
2017-11-23 16:15:33 +0700

    Initial Commit

---
+++ config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+SECRET_KEY = '40dufdiuhefhife/sdBoidshohd'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@127.0.0.1/pysimpleweb'
---
+++ simpleweb/models.py
@@ -0,0 +1,11 @@
+from simpleweb import db
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+    address = db.Column(db.Text)
+    phone = db.Column(db.String(20))
+    email = db.Column(db.String(120))
+
+    def __repr__(self):
+        return "<Name %r>" % self.name
